47fe46bdaa16bf86038ae766fcb3340085e47b82
Beatriz Saez <beatriz.saezvillarrubia@usp.ceu.es>
2026-02-15 20:18:20 +0100

    laberinto

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -4,5 +4,7 @@ import dominio.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Factorial(5)" + Mates.factorial(5));
+        System.out.println("Laberinto(3, 132343539)" + Mates.laberinto("3", "132343539"));
     }
+
 }
--- src/dominio/Mates.java
+++ src/dominio/Mates.java
@@ -8,4 +8,26 @@ public class Mates {
             return n * factorial(n-1);
     }
 
-}
+    public static int laberinto (String digito, String numero) {
+        
+        if (numero.length() == 0){
+            return 0;
+        }
+
+        else if(numero.startsWith(digito)){
+            return 1 + laberinto(digito, numero.substring(1));
+        }
+        
+        else {
+        return laberinto(digito, numero.substring(1));
+        }
+        
+    
+    }         
+        
+      
+    }
+
+
+
+
